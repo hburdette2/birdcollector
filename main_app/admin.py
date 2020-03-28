6f0023faf8a48697a53d98eec7ac13d2378c785a
Hope Burdette <hburdette@bloomberglaw.com>
2020-03-28 17:54:10 -0400

    additional functionality added

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Bird
 
+from .models import Feeding
+
 
 # Register your models here
 admin.site.register(Bird)
+
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Bird
+from .models import Feeding
+from .forms import FeedingForm
 
 
 def home(request):
@@ -18,4 +21,34 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
-    return render(request, 'birds/detail.html', {'bird': bird})
+    feeding_form = FeedingForm()
+    return render(request, 'birds/detail.html', {
+        'bird': bird,
+        'feeding_form': feeding_form
+    })
+
+
+def add_feeding(request, bird_id):
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
+
+
+class BirdCreate (CreateView):
+    model = Bird
+    fields = '__all__'
+
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds/'
